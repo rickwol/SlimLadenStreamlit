Fix NaN handling in jaaropwek_pv calculation

background_code.py
@@ -197,7 +197,7 @@ class BackgroundCode:
 
         # EV and solar
         df_MSR_profile["EV oplaad [kW]"] = df_profiles["Elaad_normal_norm. [kWh/kWh]"].copy()*msr_row["aantal_personenautos_msr"].iloc[0]*EV_adoption_perc/100*EV_jvb_per_auto*4 # (KWh per EV per year)
-        msr_row["jaaropwek_pv"] = np.where(msr_row["jaaropwek_pv"].isnan(), msr_row["n_objecten"]*0.904*900, msr_row["jaaropwek_pv"]) ##### Tijdelijke oplossing missende waardes Utrecht
+        msr_row["jaaropwek_pv"] = np.where(msr_row["jaaropwek_pv"].isna(), msr_row["n_objecten"]*0.904*900, msr_row["jaaropwek_pv"]) ##### Tijdelijke oplossing missende waardes Utrecht
         df_MSR_profile["Zonnepanelen [kW]"] = -df_profiles["ZP normalised energy [kWh/kWh]"].copy()*msr_row["jaaropwek_pv"].iloc[0]*4
         
         df_MSR_profile["Utiliteit totaal [kW]"] = df_MSR_profile["Winkel [kW]"] + df_MSR_profile["Onderwijs [kW]"] + df_MSR_profile["Kantoor_Gezondsheid [kW]"] + df_MSR_profile["Industrie [kW]"] + df_MSR_profile["Sport_Bijeenkomst_Overig [kW]"] + df_MSR_profile["Logies [kW]"]